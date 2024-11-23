85f955cf47185bbddd64fe7fb0d4b348bc582b95
sekame <94610778+sekame@users.noreply.github.com>
2024-11-24 02:49:45 +0900

    fix bug

--- Assets/playerAttack.cs
+++ Assets/playerAttack.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class playerAttack : MonoBehaviour
 {
+
+    public bool can = false;
     public float attackRadius = 5f; // 플레이어 공격 반경
     public int attackDamage = 10;  // 공격 데미지
     public LayerMask bossLayer;    // 보스 레이어 필터
 
     public void AttackBoss()
     {
+        if(can)
+        {
         // 반경 내의 모든 콜라이더 감지
         Collider[] hits = Physics.OverlapSphere(transform.position, attackRadius, bossLayer);
 
@@ -22,6 +26,7 @@ public class playerAttack : MonoBehaviour
                 boss.HandleHp();
 
             }
+        }
         }
     }
 }
