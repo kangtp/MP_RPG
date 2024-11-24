23d69e26c1468d18276a49e3c78b9580c45f9f01
kangtp <115209262+kangtp@users.noreply.github.com>
2024-11-24 18:15:28 +0900

    detatilCheck

--- Assets/Scripts/potato.cs
+++ Assets/Scripts/potato.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class potato : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class potato : MonoBehaviour
         // 충돌한 오브젝트가 "Player" 태그를 가지고 있는지 확인
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("씬이동");
+            SceneManager.LoadScene("Kingdom2");
             Destroy(this.gameObject);
         }
     }
